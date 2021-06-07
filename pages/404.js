a0f38fecf5ecdd33b14de92050aaa71842edb261
taiseen <taiseen.cse@gmail.com>
2021-06-07 23:58:22 +0600

    404 error resolve

--- pages/404.js
+++ pages/404.js
@@ -5,7 +5,7 @@ const NotFound = () => {
         <div className="not-found">
             <h1>Oooops.....</h1>
             <h2>This page cannot be found</h2>
-            <p>Go to home page <Link href="/"> <a>Home Page</a></Link></p>
+            <p>Go to home page <Link href="/"><a>Home Page</a></Link></p>
         </div>
     );
 };
--- pages/500.js
+++ pages/500.js
@@ -5,7 +5,7 @@ const ServerNotFound = () => {
         <div className="not-found">
             <h1>Oooops.....</h1>
             <h2>500 - Server-side error occurred</h2>
-            <p>Go to home page <Link href="/"> <a>Home Page</a></Link></p>
+            <p>Go to home page <Link href="/"><a>Home Page</a></Link></p>
         </div>
     );
 };
