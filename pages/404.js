765c8e2534a2a9a4fea896cd57d58298da8bbe69
taiseen <taiseen.cse@gmail.com>
2021-06-08 18:01:21 +0600

    redirect user to Home page from 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,6 +1,18 @@
 import Link from 'next/link';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 const NotFound = () => {
+
+    const router = useRouter();
+
+    // redirect user from this page to Home page after landing 3 second.
+    useEffect(() => {
+        setTimeout(() => {
+            router.push('/');
+        }, 3000);
+    }, []);
+
     return (
         <div className="not-found">
             <h1>Oooops.....</h1>
